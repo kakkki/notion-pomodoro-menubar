afe5f95330d6f2e0d831f941da4c517306c39c90
kakkki <kakki4062@gmail.com>
2022-10-16 17:27:35 +0900

    Initial Commit

---
+++ notion-pomodoro/notion_pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  notion_pomodoroApp.swift
+//  notion-pomodoro
+//
+//  Created by Atsuki Kakehi on 2022/10/16.
+//
+
+import SwiftUI
+
+@main
+struct notion_pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
