31d7f3fb64106b809598b292af7dd923015310fe
kakkki <kakki4062@gmail.com>
2022-10-22 12:57:11 +0900

    Combineを用いてカウントダウン処理を実装

--- notion-pomodoro/notion_pomodoroApp.swift
+++ notion-pomodoro/notion_pomodoroApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 /// 【Swift】SwiftUIでメニューバーアプリの作り方
 /// @see: https://qiita.com/SNQ-2001/items/7e8ac52e9e8726228806
@@ -23,16 +24,28 @@ struct SwiftUI_MenuBar_DemoApp: App {
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarItem: NSStatusItem!
     var popover = NSPopover()
+    
+//    private var cancellables: Set<AnyCancellable> = []
+    
+    let startTimerTrigger = PassthroughSubject<Void, Never>()
+    var startTimerCancellable: AnyCancellable?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         popover.behavior = .transient
-        popover.contentViewController = NSHostingController(rootView: ContentView())
+        popover.contentViewController = NSHostingController(rootView: ContentView(startTimerTrigger: startTimerTrigger))
         self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
         guard let button = self.statusBarItem.button else { return }
         // アイコンの設定
-        button.image = NSImage(systemSymbolName: "camera.macro", accessibilityDescription: nil)
+//        button.image = NSImage(systemSymbolName: "camera.macro", accessibilityDescription: nil)
+        /// テキストは設定できる
+        /// テキストは長すぎると空白になってしまうみたい
+        /// フォーカスしてるアプリケーションによっては、非表示になってしまう
+        /// メニューバーを調整するか、メニューバーにはカウントダウンだけ表示するようにするか調整が必要
+        button.title = "NP 25:00"
         // アクションの設定
         button.action = #selector(menuButtonAction(sender:))
+        
+        addObserver()
     }
 
     /// DeepLink経由での起動時のハンドリングを実行する
@@ -48,6 +61,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 //        print("host : \(url.host!)")
 //        print("port : \(url.port!)")
 //        print("query : \(url.query!)")
+    }
+    
+    var timerCancellable: AnyCancellable?
+    
+    func addObserver() {
+        print("debug0000 addObserver")
+        startTimerCancellable = startTimerTrigger.sink { [weak self] _ in
+            print("debug0000 start Timer")
+            
+            guard let button = self?.statusBarItem.button else { return }
+            
+            var remainingTime = 3
+            
+            let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+            
+            self?.timerCancellable = timer.sink { _ in
+                button.title = "NP \(self?.convertSecondsToTime(timeInSeconds: remainingTime) ?? "")"
+                guard remainingTime >= 0 else {
+                    print("debug0000 canceled")
+                    
+                    self?.timerCancellable?.cancel()
+                    
+                    // 25:00 に戻す
+                    button.title = "NP \(self?.convertSecondsToTime(timeInSeconds: 1500) ?? "")"
+                    return
+                }
+                remainingTime -= 1
+            }
+        }
+//        .store(in: &cancellables)
+    }
+    
+    func cancel() {
+        startTimerCancellable?.cancel()
+//        cancellables.forEach { $0.cancel() }
+    }
+    
+    /// @see https://www.youtube.com/watch?v=_WJzpPgHkhg
+    /// ここでの Combineを使用したカウントダウン処理を参考にした
+    func convertSecondsToTime(timeInSeconds: Int) -> String {
+        let minutes = timeInSeconds / 60
+        let seconds = timeInSeconds % 60
+        return String(format: "%02i:%02i", minutes, seconds)
     }
 }
 
